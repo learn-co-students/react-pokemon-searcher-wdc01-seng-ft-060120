642cf17d0b369d05dd8ce0bed2986b4f22cad77d
BartTheScrivner <dframe22@gmail.com>
2020-08-09 14:32:04 -0400

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,27 +1,27 @@
 import React from 'react'
 import { Card } from 'semantic-ui-react'
 
-class PokemonCard extends React.Component {
-  render() {
-    return (
-      <Card>
-        <div>
-          <div className="image">
-            <img alt="oh no!" />
-          </div>
-          <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
-          </div>
-          <div className="extra content">
-            <span>
-              <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
-            </span>
-          </div>
+function PokemonCard(props) {
+
+  return (
+    <Card>
+      <div onClick={() => {props.flipPokemonCard(props.pokemon)}}>
+        <div className="image">
+          <img src={props.pokemon.click ? props.pokemon.sprites.back : props.pokemon.sprites.front } alt="oh no!" />
         </div>
-      </Card>
-    )
-  }
+        <div className="content">
+          <div className="header">{props.pokemon.name}</div>
+        </div>
+        <div className="extra content">
+          <span>
+            <i className="icon heartbeat red" />
+            {props.pokemon.hp}
+          </span>
+        </div>
+      </div>
+    </Card>
+  )
+
 }
 
 export default PokemonCard
--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -2,21 +2,47 @@ import React from 'react'
 import { Form } from 'semantic-ui-react'
 
 class PokemonForm extends React.Component {
+  constructor(props) {
+    super (props);
+    this.state = {
+      name: "",
+      hp: 0,
+      sprites: {
+        front: "",
+        back: ""
+      }
+    };
+    this.handleSubmit = this.props.handleSubmit;
+  }
   render() {
     return (
       <div>
         <h3>Add a Pokemon!</h3>
-        <Form onSubmit={() => {console.log("submitting form...")}}>
+        <Form onSubmit={() => {this.handleSubmit(this.state)}}>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input onChange={(e) => {this.handleChange(e)}} fluid label="Name" placeholder="Name" name="name" />
+            <Form.Input onChange={(e) => {this.handleChange(e)}} fluid label="hp" placeholder="hp" name="hp" />
+            <Form.Input onChange={(e) => {this.handleImgChange(e)}} fluid label="Front Image URL" placeholder="url" name="front" />
+            <Form.Input onChange={(e) => {this.handleImgChange(e)}} fluid label="Back Image URL" placeholder="url" name="back" />
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
       </div>
     )
+  }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleImgChange = (e) => {
+    this.setState({
+      sprites: {
+        ...this.state.sprites, 
+        [e.target.name]: e.target.value
+      }
+    })
   }
 }
 
